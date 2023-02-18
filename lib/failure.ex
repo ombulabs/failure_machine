683df0ab3b98d4b07b14a2251588d296fd63ef13
Mateus Pereira <mateusdeap@gmail.com>
2023-02-18 09:33:36 -0300

    Compare messages using jaro distance

--- lib/failure.ex
+++ lib/failure.ex
@@ -8,4 +8,18 @@ defmodule Failure do
   defp atomize_keys(map) do
     Enum.into(map, %{}, fn {k, v} -> {String.to_atom(k), v} end)
   end
+
+  def message(failure) do
+    failure.exception["message"]
+  end
+
+  def sort_into(map, failure) do
+    failure_message =
+      Map.keys(map)
+      |> Enum.find(Failure.message(failure), fn message ->
+        String.jaro_distance(message, Failure.message(failure)) > 0.8
+      end)
+
+    Map.update(map, failure_message, [failure], fn failure_list -> failure_list ++ [failure] end)
+  end
 end
